5bc4bc82596677f42f6bfd439e4e0d62ff506e1a
mikunilabo <k-wada@mikunilabo.com>
2018-07-11 11:54:41 +0900

    フロントエンドビルド環境を整備

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,39 +11,11 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    {{-- ※後にsassへの移行時に以下メディアクエリCSS等も移行して要削除 --}}
-    <style>
-        body {
-            font: 16px/1.6 "メイリオ", Meiryo, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", Osaka, "ＭＳ Ｐゴシック", "MS PGothic", Helvetica, Arial, Sans-Serif;
-            animation: fadeIn 1.5s ease 0s 1 normal;
-            -webkit-animation: fadeIn 1.5s ease 0s 1 normal;
-        }
-
-        .navbar-fixed-left {
-          box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.4);
-        }
-
-        @media only screen and (min-width : 768px) {
-            .dropdown:hover .dropdown-menu {
-                display: block;
-            }
-        }
-
-        @media (min-width:768px){.container{width:503px}}@media (min-width:992px){.container{width:723px}}@media (min-width:1200px){.container{width:923px}}@media (min-width:1432px){.container{width:1170px}}body{padding-top:70px}.navbar-fixed-left,.navbar-fixed-right{position:fixed;top:0;width:100%;z-index:1030}@media (min-width:768px){.navbar-fixed-left,.navbar-fixed-right{width:232px;height:100vh;border-radius:0}.navbar-fixed-left .container,.navbar-fixed-left .container-fluid,.navbar-fixed-right .container,.navbar-fixed-right .container-fluid{padding-right:0;padding-left:0;width:auto}.navbar-fixed-left .navbar-header,.navbar-fixed-right .navbar-header{float:none;padding-left:15px;padding-right:15px}.navbar-fixed-left .navbar-collapse,.navbar-fixed-right .navbar-collapse{padding-right:0;padding-left:0;max-height:none}.navbar-fixed-left .navbar-collapse .navbar-nav,.navbar-fixed-right .navbar-collapse .navbar-nav{float:none!important}.navbar-fixed-left .navbar-collapse .navbar-nav>li,.navbar-fixed-right .navbar-collapse .navbar-nav>li{width:100%}.navbar-fixed-left .navbar-collapse .navbar-nav>li.dropdown .dropdown-menu,.navbar-fixed-right .navbar-collapse .navbar-nav>li.dropdown .dropdown-menu{top:0}.navbar-fixed-left .navbar-collapse .navbar-nav.navbar-right,.navbar-fixed-right .navbar-collapse .navbar-nav.navbar-right{margin-right:0}}@media (min-width:768px){body{padding-top:0;margin-left:232px}.navbar-fixed-left{right:auto!important;left:0!important;border-width:0 1px 0 0!important}.navbar-fixed-left .dropdown .dropdown-menu{left:100%;right:auto;border-radius:0 3px 3px 0}.navbar-fixed-left .dropdown .dropdown-toggle .caret{border-top:4px solid transparent;border-left:4px solid;border-bottom:4px solid transparent;border-right:none}}
-
-        @keyframes fadeIn {
-            0% {opacity: 0}
-            100% {opacity: 1}
-        }
-
-        @-webkit-keyframes fadeIn {
-            0% {opacity: 0}
-            100% {opacity: 1}
-        }
-    </style>
-    {{-- ※ここまで --}}
+    @if (file_exists(public_path('mix-manifest.json')))
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    @else
+        <link href="{{ asset('css/default.css') }}" rel="stylesheet">
+    @endif
 </head>
 <body>
     <div id="app">
@@ -55,7 +27,13 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    @if (file_exists(public_path('mix-manifest.json')))
+        <script type="text/javascript" src="{{ mix('js/manifest.js') }}"></script>
+        <script type="text/javascript" src="{{ mix('js/vendor.js') }}"></script>
+        <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+    @else
+        <script type="text/javascript" src="{{ asset('js/default.js') }}"></script>
+    @endif
 
     @yield('scripts')
 </body>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,16 @@ let mix = require('laravel-mix');
  |
  */
 
+// Js
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .extract(['vue']);
+
+//Sass
+mix.sass('resources/assets/sass/app.scss', 'public/css');
+
+// Versioning
+if (mix.inProduction()) {
+    mix.version();
+} else {
+	mix.sourceMaps();
+}
