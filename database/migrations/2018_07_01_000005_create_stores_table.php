9599300347a20275d723f29204a15fcf447de4a6
mikunilabo <k-wada@mikunilabo.com>
2018-07-14 13:35:48 +0900

    ref #1 店舗テーブルマイグレーション作成

---
+++ database/migrations/2018_07_01_000005_create_stores_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /** @var string */
+    private $table = 'stores';
+
+    /**
+     * @return void
+     */
+    public function up()
+    {
+        try {
+            Schema::create($this->table, function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('company_id')->nullable()->comment('企業ID');
+                $table->unsignedInteger('prefecture_id')->nullable()->comment('都道府県ID');
+
+                $table->string('name')->nullable()->comment('名称');
+                $table->string('kana')->nullable()->comment('フリガナ');
+                $table->string('postal_code')->nullable()->comment('郵便番号');
+                $table->text('address')->nullable()->comment('住所');
+                $table->text('building_name')->nullable()->comment('建物名');
+                $table->string('tel')->nullable()->comment('TEL');
+                $table->string('fax')->nullable()->comment('FAX');
+                $table->string('email')->nullable()->comment('E-Mail');
+
+                $table->boolean('payment_flag')->default(false)->comment('入金フラグ');
+                $table->timestamp('starts_at')->nullable()->comment('サービス開始日時');
+                $table->timestamp('ends_at')->nullable()->comment('サービス終了日時');
+                $table->unsignedInteger('user_limit')->default(0)->comment('ユーザーログイン上限');
+                $table->unsignedInteger('login_status_cnt')->default(0)->comment('ログイン状態カウント');
+
+                $table->timestamps();
+                $table->softDeletes();
+
+                $table->foreign('company_id')
+                    ->references('id')
+                    ->on('companies');
+
+                $table->foreign('prefecture_id')
+                    ->references('id')
+                    ->on('prefectures');
+            });
+
+            DB::statement(sprintf("ALTER TABLE %s%s COMMENT '店舗'", DB::getTablePrefix(), $this->table));
+        } catch (\Exception $e) {
+            report($e);
+            $this->down();
+            dd($e->getMessage());
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->table);
+    }
+}
