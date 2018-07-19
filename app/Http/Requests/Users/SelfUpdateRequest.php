8ba4bee8bba2f8777b7f7952bcfd749fae5598d6
mikunilabo <k-wada@mikunilabo.com>
2018-07-19 17:10:26 +0900

    ref #12 コアレイヤからユースケースを経てサービスレイヤへ処理を移譲するパターンを実験するべく、ユーザー自身の情報を更新するためのフローを開発（フォームリクエスト）

---
+++ app/Http/Requests/Users/SelfUpdateRequest.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Requests\Users;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class SelfUpdateRequest extends FormRequest
+{
+    /**
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:191',
+            ],
+            'email' => [
+                'required',
+                'string',
+                'max:100',
+                Rule::unique('users')->ignore(auth()->user()->getAuthIdentifier()),
+            ],
+            'password' => [
+                'nullable',
+                'string',
+                'min:8',
+                'max:16',
+                'confirmed',
+            ],
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes(): array
+    {
+        return \Lang::get('attributes.auth');
+    }
+}
