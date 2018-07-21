6c0bd271a83cc585122018882f1c6ca467536c3e
mikunilabo <k-wada@mikunilabo.com>
2018-07-21 23:46:13 +0900

    ref #13 認可判定ロジックをミドルウェア以外でも利用予定のため、ゲートとして切り出した

--- app/Http/Middleware/Authorize.php
+++ app/Http/Middleware/Authorize.php
@@ -3,48 +3,34 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
-use Domain\Contracts\Users\GetUserInterface;
 use Closure;
-use Domain\Models\Permission;
 use Illuminate\Contracts\Auth\Factory as Auth;
-use Illuminate\Validation\UnauthorizedException;
 
 class Authorize
 {
     /** @var Auth */
     private $auth;
 
-    /** @var GetUserInterface */
-    private $usersService;
-
     /**
      * @param Auth $auth
      */
-    public function __construct(Auth $auth, GetUserInterface $usersService)
+    public function __construct(Auth $auth)
     {
         $this->auth = $auth;
-        $this->usersService = $usersService;
     }
 
     /**
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string  $arg
+     * @param  string  $args
      * @return mixed
      */
-    public function handle($request, Closure $next, string $arg = '')
+    public function handle($request, Closure $next, string $args = '')
     {
-        $id = $this->auth->user()->id;
-        $permissions = $this->usersService->findById($id)->permissions();
-
-        foreach (explode('|', $arg) as $permission) {
-            if ($permissions->containsStrict(function (Permission $item) use ($permission) {
-                return $item->slug() === $permission;
-            })) {
-                return $next($request);
-            }
+        if ($this->auth->user()->can('authorize', explode('|', $args))) {
+            return $next($request);
         }
 
-        throw new UnauthorizedException('This action is Unauthorized.', 403);
+        abort(403, 'This action is Unauthorized.');
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,30 +1,41 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Domain\Contracts\Users\GetUserInterface;
+use Domain\Models\Permission;
 
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
-     *
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+//         'App\SomeModel::class' => 'App\Policies\SomeModelPolicy::class',
     ];
 
     /**
-     * Register any authentication / authorization services.
-     *
      * @return void
      */
     public function boot()
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('authorize', function ($user, ...$args): bool {
+            $args = is_array($args) ? $args : [$args];
+            $permissions = app(GetUserInterface::class)->findById($user->id)->permissions();
+
+            foreach ($args as $permission) {
+                if ($permissions->containsStrict(function (Permission $item) use ($permission) {
+                    return $item->slug() === $permission;
+                })) {
+                    return true;
+                }
+            }
+            return false;
+        });
     }
 }
