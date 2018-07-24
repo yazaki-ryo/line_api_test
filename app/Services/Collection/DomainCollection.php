d8d552e732d013d70fbd565763e8b3f6a11f8d4e
mikunilabo <k-wada@mikunilabo.com>
2018-07-24 23:53:45 +0900

    ドメイン固有のコレクションメソッドをオーバーライドクラスに独自定義

--- app/Services/Collection/DomainCollection.php
+++ app/Services/Collection/DomainCollection.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Collection;
 
 final class DomainCollection extends Collection
 {
+    /**
+     * @return array
+     */
+    public function pluckNamesByIds(): array
+    {
+        return $this->map(function ($item) {
+            return [
+                'id'   => $item->id(),
+                'name' => $item->name(),
+            ];
+        })->pluck('name', 'id')->all();
+    }
 }
