8ffbadbc8be573aa94f4a8fdd51117255af018ee
mikunilabo <k-wada@mikunilabo.com>
2018-07-24 22:15:17 +0900

    ログイン画面以外のカラム幅を最大にしておく

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -23,7 +23,7 @@
         </div>
 
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-12 col-md-offset-0">
                 <div class="panel panel-default">
                     <div class="panel-heading"> @lang ('Please enter necessary items.') </div>
 
