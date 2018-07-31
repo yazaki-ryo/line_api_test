557d7d67a3e5623b0fe0bdde3915c90af9f45b7f
mikunilabo <k-wada@mikunilabo.com>
2018-07-31 16:14:40 +0900

    ref #30 お知らせ表示を記事一覧風レイアウトでイメージ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,33 +23,99 @@
         </div>
 
         <div class="row">
-            <div class="col-md-6 col-md-offset-0">
-                <div class="well" style="height:220px;overflow:auto;">
+            <div class="col-md-12 col-md-offset-0">
+                <div class="well" style="height: 220px; overflow: auto;">
                     <p class="lead">■@lang ('elements.labels.notice')</p>
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object thumbnail" src="http://placehold.jp/16/ddd/666/64x64.png?text=64x64" alt="">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Media heading</h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object thumbnail" src="http://placehold.jp/16/ddd/666/64x64.png?text=64x64" alt="">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Media heading</h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="col-md-6 col-md-offset-0">
                 <div class="well" style="height:220px;overflow:auto;">
                     <p class="lead">■@lang ('elements.resources.customers')@lang ('elements.labels.information')</p>
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object thumbnail" src="http://placehold.jp/16/ddd/666/64x64.png?text=64x64" alt="">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Media heading</h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="col-md-6 col-md-offset-0">
                 <div class="well" style="height:220px;overflow:auto;">
                     <p class="lead">■@lang ('elements.resources.stores')@lang ('elements.labels.information')</p>
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object thumbnail" src="http://placehold.jp/16/ddd/666/64x64.png?text=64x64" alt="">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Media heading</h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="col-md-6 col-md-offset-0">
                 <div class="well" style="height:220px;overflow:auto;">
                     <p class="lead">■@lang ('elements.resources.users')@lang ('elements.labels.information')</p>
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object thumbnail" src="http://placehold.jp/16/ddd/666/64x64.png?text=64x64" alt="">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Media heading</h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="col-md-6 col-md-offset-0">
                 <div class="well" style="height:220px;overflow:auto;">
                     <p class="lead">■@lang ('elements.resources.companies')@lang ('elements.labels.information')</p>
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object thumbnail" src="http://placehold.jp/16/ddd/666/64x64.png?text=64x64" alt="">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Media heading</h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
