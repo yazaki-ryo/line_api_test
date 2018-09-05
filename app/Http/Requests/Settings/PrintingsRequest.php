3660f177eebd9a4e0e04971e7109835729a33f70
mikunilabo <k-wada@mikunilabo.com>
2018-09-05 12:53:09 +0900

    ref #39 印刷設定フォームのバリデーション調整

--- app/Http/Requests/Settings/PrintingsRequest.php
+++ app/Http/Requests/Settings/PrintingsRequest.php
@@ -21,18 +21,200 @@ class PrintingsRequest extends FormRequest
      */
     public function rules(): array
     {
-        $rules = [];
+        $i = $this->segment(4);
+        if (! is_numeric($i)) {
+            throw new \InvalidArgumentException('The transmitted setting ID is invalid.');
+        }
 
-        for ($i = 1; $i < 4; $i++) {
-            $rules["name_{$i}"] = [
-                'sometimes',
+        return [
+            sprintf('name_%s', $i) => [
                 'required',
                 'string',
                 'max:191',
-            ];
-        }
+            ],
 
-        return $rules;
+            /**
+             * Postalcode
+             */
+            sprintf('pc_position_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.positions'))),
+            ],
+            sprintf('pc_frame_%s', $i) => [
+                'required',
+                'boolean',
+            ],
+            sprintf('pc_symbol_%s', $i) => [
+                'required',
+                'boolean',
+            ],
+            sprintf('pc_x_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('pc_y_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('pc_font_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fonttypes'))),
+            ],
+            sprintf('pc_font_size_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fontsizes'))),
+            ],
+
+            /**
+             * Address
+             */
+            sprintf('address_x_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('address_y_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('address_font_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fonttypes'))),
+            ],
+            sprintf('address_font_size_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fontsizes'))),
+            ],
+
+            /**
+             * Name
+             */
+            sprintf('name_x_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('name_y_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('name_font_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fonttypes'))),
+            ],
+            sprintf('name_font_size_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fontsizes'))),
+            ],
+
+            /**
+             * Senders
+             */
+            sprintf('sender_flag_%s', $i) => [
+                'required',
+                'boolean',
+            ],
+
+            /**
+             * Sender postalcode
+             */
+            sprintf('pc_symbol_%s', $i) => [
+                'required',
+                'boolean',
+            ],
+            sprintf('sender_pc_x_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('sender_pc_y_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('sender_pc_font_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fonttypes'))),
+            ],
+            sprintf('sender_pc_font_size_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fontsizes'))),
+            ],
+
+            /**
+             * Sender address
+             */
+            sprintf('sender_address_x_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('sender_address_y_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('sender_address_font_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fonttypes'))),
+            ],
+            sprintf('sender_address_font_size_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fontsizes'))),
+            ],
+
+            /**
+             * Sender name
+             */
+            sprintf('sender_name_x_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('sender_name_y_%s', $i) => [
+                'required',
+                'numeric',
+                'max:999',
+            ],
+            sprintf('sender_name_font_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fonttypes'))),
+            ],
+            sprintf('sender_name_font_size_%s', $i) => [
+                'required',
+                'string',
+                'max:191',
+                Rule::in(array_keys(config('pdf.fontsizes'))),
+            ],
+        ];
     }
 
     /**
