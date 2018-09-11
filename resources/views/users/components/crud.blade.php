3716d7e7f561046906b31e37e7d7a1964ac6b128
mikunilabo <k-wada@mikunilabo.com>
2018-09-11 16:19:06 +0900

    ref #41 * 環境判定を行うbladeディレクティブを追加 * localのみアバター機能を表示しておく

--- resources/views/users/components/crud.blade.php
+++ resources/views/users/components/crud.blade.php
@@ -60,30 +60,30 @@
     </div>
 </div>
 
-{{--
-@set ($field, 'avatar')
-@set ($field2, 'drop_avatar')
-<div class="form-group{{ $errors->has($field) ? ' has-error' : '' }}">
-    <label for="{{ $field }}" class="col-md-4 control-label">
-        @lang ("attributes.users.{$field}")
-    </label>
+@env ('local')
+    @set ($field, 'avatar')
+    @set ($field2, 'drop_avatar')
+    <div class="form-group{{ $errors->has($field) ? ' has-error' : '' }}">
+        <label for="{{ $field }}" class="col-md-4 control-label">
+            @lang ("attributes.users.{$field}")
+        </label>
 
-    <div class="col-md-6 form-control-static">
-        @include ('users.components.avatars')
+        <div class="col-md-6 form-control-static">
+            @include ('users.components.avatars')
 
-        {!! Form::file($field, null, ['class' => 'form-control', 'id' => $field, 'placeholder' => '']) !!}
-        {!! Form::hidden('MAX_FILE_SIZE', 2097152) !!}<!-- いずれ設定値から取得 -->
-        {!! $errors->first($field, '<span class="glyphicon glyphicon-remove form-control-feedback"></span><span class="help-block"><strong>:message</strong></span>') !!}
+            {!! Form::file($field, null, ['class' => 'form-control', 'id' => $field, 'placeholder' => '']) !!}
+            {!! Form::hidden('MAX_FILE_SIZE', 2097152) !!}<!-- いずれ設定値から取得 -->
+            {!! $errors->first($field, '<span class="glyphicon glyphicon-remove form-control-feedback"></span><span class="help-block"><strong>:message</strong></span>') !!}
 
-        @if ($row->avatars()->count())
-            <div class="checkbox">
-                <label>{!! Form::checkbox($field2, 1, old($field2), ['class' => '', 'id' => $field2]) !!} @lang ('Delete the current image.')</label>
-                {!! $errors->first($field2, '<span class="glyphicon glyphicon-remove form-control-feedback"></span><span class="help-block"><strong>:message</strong></span>') !!}
-            </div>
-        @endif
+            @if ($row->avatars()->count())
+                <div class="checkbox">
+                    <label>{!! Form::checkbox($field2, 1, old($field2), ['class' => '', 'id' => $field2]) !!} @lang ('Delete the current image.')</label>
+                    {!! $errors->first($field2, '<span class="glyphicon glyphicon-remove form-control-feedback"></span><span class="help-block"><strong>:message</strong></span>') !!}
+                </div>
+            @endif
+        </div>
     </div>
-</div>
---}}
+@endenv
 
 @set ($field, 'password')
 <div class="form-group{{ $errors->has($field) ? ' has-error' : '' }}">
