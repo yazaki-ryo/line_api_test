decb214df2ede509efb5f75b376a26039e1ef5f8
mikunilabo <k-wada@mikunilabo.com>
2018-09-22 22:27:51 +0900

    eny errorsブレードをalertブレードとセットで全ビューに設置

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,7 @@
         <div class="row">
             <div class="col-md-12 col-md-offset-0">
                 @include ('components.parts.alerts')
+                @include ('components.parts.any_errors')
             </div>
         </div>
 
