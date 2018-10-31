005c72203c2dd3449bf1faa250db032aaefd607c
mikunilabo <k-wada@mikunilabo.com>
2018-10-31 15:40:23 +0900

    test vue.js

---
+++ resources/assets/js/components/vue/Test.vue
@@ -0,0 +1,32 @@
+<template>
+    <button @click="count++">You clicked me {{ count }} times.</button>
+</template>
+
+<script>
+    export default {
+        /**
+         * Prepare the component (Vue 2.x).
+         */
+        mounted() {
+//            console.log('Component mounted.')
+        },
+
+        /**
+         * The component's data.
+         */
+        data() {
+            return {
+                count: 0
+            };
+        },
+
+        /**
+         * The component's methods.
+         */
+        methods: {
+            //
+        }
+    }
+</script>
+
+<style scoped></style>
