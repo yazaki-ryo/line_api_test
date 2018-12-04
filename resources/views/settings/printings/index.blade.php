64fb0d5e74441f8949ea2e1995e7c7fd995d553a
k.togei <k.togei@n-3.so>
2018-12-04 15:49:03 +0900

    タブメニューの調整

--- resources/views/settings/printings/index.blade.php
+++ resources/views/settings/printings/index.blade.php
@@ -7,34 +7,30 @@
 @endsection
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 col-md-offset-0">
-                <div class="page-header">
-                    	<h1 class="h2">@lang ('elements.words.print')@lang ('elements.words.settings')
-                </div>
-            </div>
-        </div>
-
+    <div class="nav-tabs-container side-by-side wrap">
+        <p class="page-title">
+            <i class="fas fa-angle-double-right"></i>
+            @lang ('elements.words.print')@lang ('elements.words.settings')
+        </p>
+        <ul class="nav nav-tabs">
+            @foreach ($rows as $key => $item)
+                <li class="{{ \Util::activatable($errors, sprintf('settings_printings_request_%s', $key), $key === 1 ? true : false) }}">
+                    <a href="{{ sprintf('#setting%s-tab', $key) }}" data-toggle="tab">
+                        {{ is_null($rows[$key]) ? sprintf('%s%s', __('elements.words.settings'), $key) : $rows[$key]->name() }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="container pt-150">
         <div class="row">
             <div class="col-md-12 col-md-offset-0">
                 @include ('components.parts.alerts')
                 @include ('components.parts.any_errors')
             </div>
         </div>
-
         <div class="row">
             <div class="col-md-12 col-md-offset-0">
-                <ul class="nav nav-tabs">
-                    @foreach ($rows as $key => $item)
-                        <li class="{{ \Util::activatable($errors, sprintf('settings_printings_request_%s', $key), $key === 1 ? true : false) }}">
-                            <a href="{{ sprintf('#setting%s-tab', $key) }}" data-toggle="tab">
-                                {{ is_null($rows[$key]) ? sprintf('%s%s', __('elements.words.settings'), $key) : $rows[$key]->name() }}
-                            </a>
-                        </li>
-                    @endforeach
-                </ul>
-
                 <div class="tab-content">
                     @foreach ($rows as $key => $item)
                         <div class="tab-pane fade in pt-10 {{ \Util::activatable($errors, sprintf('settings_printings_request_%s', $key), $key === 1 ? true : false) }}" id="{{ sprintf('setting%s-tab', $key) }}">
