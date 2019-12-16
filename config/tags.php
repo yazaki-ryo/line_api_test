4b3299a4827dbdddd9b758a4a31affd6219550bb
Keiju Togei <info@togei-sf.co.jp>
2019-12-16 15:25:54 +0900

    タグの表記が英語になっていたので日本語(色名)に修正しました。

--- config/tags.php
+++ config/tags.php
@@ -3,12 +3,12 @@ declare(strict_types=1);
 
 return [
     'labels' => [
-        'default' => 'デフォルト',
-        'primary' => 'プライマリ',
-        'info'    => 'インフォメーション',
-        'success' => 'サクセス',
-        'warning' => 'ワーニング',
-        'danger'  => 'デンジャー',
+        'default' => '灰色',
+        'primary' => '青色',
+        'info'    => '水色',
+        'success' => '緑色',
+        'warning' => 'オレンジ色',
+        'danger'  => '赤色',
     ],
 
 ];
