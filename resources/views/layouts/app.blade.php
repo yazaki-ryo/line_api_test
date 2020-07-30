22174f702e010501d7726f216af4e631a4cf5ce9
Keiju Togei <info@togei-sf.co.jp>
2020-07-30 10:21:40 +0900

    グーグルのインデックスに掲載されていたのでブロック処理を追加しました。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="oMMEtCI2LexgSp6HmvF-P0rrX8oqGwdzis4hrJeinBg" />
+    <meta name="robots" content="noindex">
 
     @yield('meta')
 
