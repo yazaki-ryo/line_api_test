e68952f1d9bb8e051ac96f5d36009a28ef074978
矢崎亮太 <yazaki@yazakiryoutanoMacBook-Pro.local>
2021-04-26 01:26:30 +0900

    スタイルを調整

--- resources/views/line/contact/head.blade.php
+++ resources/views/line/contact/head.blade.php
@@ -7,7 +7,7 @@
   -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="{{asset('css/app.css?param=1')}}">
+    <link rel="stylesheet" href="{{asset('css/line_app.css?param=1')}}">
     <link rel="stylesheet" href="{{asset('css/picker/default.css')}}">
     <link rel="stylesheet" href="{{asset('css/picker/default.date.css')}}">
     <link rel="stylesheet" href="{{asset('js/jquery-timepicker/jquery.timepicker.min.css')}}">
